8502bcf5dacbf083acffe65d3fe32d7d820d5e54
Saturnovich <konstantin.kachurenko@gmail.com>
2015-05-26 12:47:37 +0300

    Update jquery.tiny-highlight.js

--- jquery.tiny-highlight.js
+++ jquery.tiny-highlight.js
@@ -1,22 +1,15 @@
 (function($){ "use strict";
 
-	$.tinyHighlight = function(elem) {
-
-		this.$element = $(elem);
-		var code = this.$element.html();
-		
-		code = code.replace(/((?:<!--.*?-->)|(?:\/\*.*?\*\/))/gm, '<span class="tiny-highlight-comment">$1</span>');
-
-		this.$element.html(code);
-	};
+  $.tinyHighlight = function(elem) {
+  };
 	
-	$.tinyHighlight.prototype.$element = null;
+  $.tinyHighlight.prototype.$element = null;
 	
-    /** Регистрация плагина jQuery */ 
-    $.fn.tinyHighlight = function() {
-        return this.each(function(i, elem) {
-            (new $.tinyHighlight(elem));
-        });
-    };
+  /** Регистрация плагина jQuery */ 
+  $.fn.tinyHighlight = function() {
+    return this.each(function(i, elem) {
+      (new $.tinyHighlight(elem));
+    });
+  };
 	
 })(jQuery);
