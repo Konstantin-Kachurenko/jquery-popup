dd2c8adc8c860cb0648622ad27cff6f79906184d
Konstantin Kachurenko <konstantin.kachurenko@gmail.com>
2015-05-26 09:38:18 +0300

    add tinyhighlight

---
+++ qjuery.tiny-highlight.js
@@ -0,0 +1,22 @@
+(function($){ "use strict";
+
+	$.tinyHighlight = function(elem) {
+
+		this.$element = $(elem);
+		var code = this.$element.html();
+		
+		code = code.replace(/((?:<!--.*?-->)|(?:\/\*.*?\*\/))/ig, '<span class="tiny-highlight-comment">$1</span>');
+
+		this.$element.html(code);
+	};
+	
+	$.tinyHighlight.prototype.$element = Null;
+	
+    /** Регистрация плагина jQuery */ 
+    $.fn.tinyHighlight = function() {
+        return this.each(function(i, elem) {
+            (new $.tinyHighlight(elem));
+        });
+    };
+	
+})(jQuery);
